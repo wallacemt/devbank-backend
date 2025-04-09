1891c9f3bf6a4d5e38788667ae2e897b2893b887
Wallace Santana <wallacesantanak0@gmail.com>
2025-04-09 15:49:07 -0300

    (feat): Add Outher App Properties

--- src/main/java/com/devbank/DevBank/DevBankApplication.java
+++ src/main/java/com/devbank/DevBank/DevBankApplication.java
@@ -14,10 +14,13 @@ public class DevBankApplication {
 		System.setProperty("spring.mail.password", dotenv.get("SPRING_MAIL_PASSWORD"));
 		System.setProperty("spring.mail.host", dotenv.get("SPRING_MAIL_HOST"));
 		System.setProperty("spring.mail.port", dotenv.get("SPRING_MAIL_PORT"));
+		System.setProperty("spring.profiles.active", dotenv.get("SPRING_PROFILES_ACTIVE"));
 
-		System.setProperty("spring.datasource.url", dotenv.get("SPRING_DATASOURCE_URL"));
-		System.setProperty("spring.datasource.username", dotenv.get("SPRING_DATASOURCE_USERNAME"));
-		System.setProperty("spring.datasource.password", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+		if ("dev".equals(dotenv.get("SPRING_PROFILES_ACTIVE"))) {
+		    System.setProperty("spring.datasource.url", dotenv.get("SPRING_DATASOURCE_URL"));
+		    System.setProperty("spring.datasource.username", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+		    System.setProperty("spring.datasource.password", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+		}
 
 		SpringApplication.run(DevBankApplication.class, args);
 	}
